1d2bb28e1d0649a8931c2d620232f1086bb02ddf
Nathan Smith <2020nsmith@gmail.com>
2021-02-17 13:54:10 -0500

    first push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubPracticeRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a practice repository for Git Hub.");
+        }
+    }
+}
